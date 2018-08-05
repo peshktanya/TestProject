4f9515aa1386d2736db7d62a49f04db2b02de0f2
Tetiana <peshkovat@ukr.net>
2018-08-05 20:47:08 +0300

    Add main class

--- src/test/java/NewClassOk.java
+++ src/test/java/NewClassOk.java
@@ -1,5 +1,8 @@
 public class NewClassOk {
     public static void main(String[] args) {
         System.out.print("Hello");
+        int sum;
+        sum = 2+ 2;
+        System.out.print(sum);
     }
 }
